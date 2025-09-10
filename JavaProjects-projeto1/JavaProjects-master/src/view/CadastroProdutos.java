072bcd68086f526a518ae0459228338041ad8fa0
Gustavo Grocelli Rosa <rosaguga07@gmail.com>
2025-09-10 09:09:32 -0300

    v2 projeto1PDS

---
+++ JavaProjects-projeto1/JavaProjects-master/src/view/CadastroProdutos.java
@@ -0,0 +1,65 @@
+package view;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JComboBox;
+
+public class CadastroProdutos extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JTable table;
+
+	/**
+	 * Create the panel.
+	 */
+	public CadastroProdutos(Janelas j) {
+		setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 450, 22);
+		add(menuBar);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"cadastro", null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		table.setBounds(0, 27, 450, 273);
+		add(table);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(0, 2, 110, 20);
+		add(comboBox);
+
+	}
+}
---
+++ JavaProjects-projeto1/JavaProjects-master/src/view/Janelas.java
@@ -0,0 +1,58 @@
+package view;
+
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Janelas extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+
+	private CardLayout cardLayout;
+	
+	private Login  login;
+	private CadastroProdutos cadastroProdutos;
+	private ComprarProdutos  comprarProdutos;
+	
+	public static final String LOGIN_PANEL = "telaLogin";
+	public static final String CADASTROPRODUTOS_PANEL = "telaCadastroProdutos";
+	public static final String COMPRAR_PANEL = "telaCompra";
+	
+    private String currentUser;
+	
+    public Janelas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		cardLayout = new CardLayout();
+	    
+		contentPane = new JPanel(cardLayout);
+	    contentPane.setPreferredSize(new Dimension(500, 200));
+	    
+	   login = new Login(this);
+	   cadastroProdutos = new CadastroProdutos(this);
+	   comprarProdutos = new ComprarProdutos(this);
+	   
+	   contentPane.add(login, LOGIN_PANEL);
+	   contentPane.add(cadastroProdutos, CADASTROPRODUTOS_PANEL);
+	   contentPane.add(comprarProdutos, COMPRAR_PANEL);
+	   
+		setContentPane(contentPane);
+	   
+		mostrarTela(LOGIN_PANEL);
+	   
+	}
+    public void mostrarTela(String panelName) {
+        cardLayout.show(contentPane, panelName);
+    }
+
+}
