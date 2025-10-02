3c41b75cb9a7c18ecac6e440f83b3a39e68ae637
GugaRosa456 <rosaguga07@gmail.com>
2025-10-02 16:15:44 -0300

    tela de login v2

--- src/view/Login.java
+++ src/view/Login.java
@@ -15,12 +15,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JSeparator;
 
 public class Login extends JPanel {
 	private JTextField textFieldNome;
 	private JTextField textFieldSenha;
     private JButton Entrar;
     private JButton Cadastro;
+    private JSeparator separator;
 
 	/**
 	 * Create the panel.
@@ -30,45 +32,53 @@ public class Login extends JPanel {
 		setLayout(null);
 		
 		textFieldNome = new JTextField();
-		textFieldNome.setBounds(20, 59, 301, 20);
+		textFieldNome.setBounds(20, 77, 301, 20);
 		add(textFieldNome);
 		textFieldNome.setColumns(10);
 		
 		textFieldSenha = new JTextField();
 		textFieldSenha.setColumns(10);
-		textFieldSenha.setBounds(20, 131, 301, 20);
+		textFieldSenha.setBounds(20, 144, 301, 20);
 		add(textFieldSenha);
 		
 		JLabel lblNewLabel = new JLabel("Nome");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel.setBounds(20, 34, 91, 14);
+		lblNewLabel.setBounds(153, 52, 43, 14);
 		add(lblNewLabel);
 		
 		JLabel lblSenha = new JLabel("Senha");
 		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblSenha.setBounds(20, 106, 91, 14);
+		lblSenha.setBounds(153, 119, 43, 14);
 		add(lblSenha);
 		
 		Entrar = new JButton("Entrar");
-		Entrar.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		Entrar.setBounds(123, 183, 89, 23);
+		Entrar.setFont(Entrar.getFont().deriveFont(Entrar.getFont().getStyle() | Font.BOLD));
+		Entrar.setBounds(130, 194, 89, 20);
 		add(Entrar);
 		
 		
 		Cadastro = new JButton("Cadastre-se");
+		Cadastro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		Cadastro.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		Cadastro.setBounds(123, 266, 89, 23);
+		Cadastro.setBounds(201, 282, 89, 23);
 		add(Cadastro);
 		
 		
 		JLabel lblNoTemUm = new JLabel("Não tem um conta ainda?");
 		lblNoTemUm.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNoTemUm.setBounds(98, 232, 146, 14);
+		lblNoTemUm.setBounds(20, 285, 146, 14);
 		add(lblNoTemUm);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 350, 22);
 		add(menuBar);
+		
+		separator = new JSeparator();
+		separator.setBounds(0, 247, 350, 2);
+		add(separator);
 	}
 	
 		public String getNome() {
